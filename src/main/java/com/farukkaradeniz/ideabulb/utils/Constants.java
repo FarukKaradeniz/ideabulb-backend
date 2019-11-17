6b3c28aee4de33a2b18084aa0fe738d9a3de5707
Faruk Karadeniz <krdnzomer@gmail.com>
2019-11-17 17:49:11 +0300

    Update Constants file

--- src/main/java/com/farukkaradeniz/ideabulb/utils/Constants.java
+++ src/main/java/com/farukkaradeniz/ideabulb/utils/Constants.java
@@ -2,4 +2,6 @@ package com.farukkaradeniz.ideabulb.utils;
 
 public class Constants {
     public static final String API_V1 = "/api/v1";
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_USER = "USER";
 }
